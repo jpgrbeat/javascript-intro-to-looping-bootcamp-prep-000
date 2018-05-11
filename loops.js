f105132c6192f01e2e445c4e8bf4d0cf29e6d06f
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-05-11 02:08:27 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(arr){
+
+  for(let i = 0; i < 25; i++){
+    if(i===1){
+      arr[i] = `I am ${i} strange loop.`;
+    }
+    else{
+      arr[i] = `I am ${i} strange loops.`
+    }
+  
+  }
+  return arr;
+}
