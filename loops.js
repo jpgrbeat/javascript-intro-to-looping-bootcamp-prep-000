2a6493d2bb3104435e16d252ec5a10a9067dbe39
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-05-11 03:40:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(arr){
 
   for(let i = 0; i < 25; i++){
     if(i===1){
-      arr[i] = `I am ${i} strange loop.`;
+      arr.push(`I am ${i} strange loop.`);
     }
     else{
       arr[i] = `I am ${i} strange loops.`
